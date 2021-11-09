abe0cddf546560e55b37637711de24cffae3f1c7
devinitiald_ <rddachmad@gmail.com>
2021-11-09 16:34:37 +0700

    finish section

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Person struct {
+	XMLName   xml.Name `xml:"person"`
+	FirstName string   `xml:"firstName, attr"`
+	LastName  string   `xml:"lastName, attr"`
+}
+
+func IndexHandler(c *gin.Context) {
+	c.XML(200, Person{FirstName: "Ridho", LastName: "Achmad"})
+}
+
+// func IndexHandler(c *gin.Context) {
+// 	name := c.Params.ByName("name")
+// 	c.JSON(200, gin.H{
+// 		"message": "Test Gin FW " + name,
+// 	})
+// }
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Test Gin FW",
-		})
-	})
+	router.GET("/", IndexHandler)
 	// router.Run(":8080")
 	router.Run()
 }
